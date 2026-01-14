5c66b081a4ad348a46bc2860f33eee9b5cf7b64a
Monica Santamaria <santamariamonica31@gmail.com>
2026-01-14 00:54:16 -0500

    Asignar URL con parametro

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        path('', views.home, name="home"),
-] 
+    path('drinks/<str:drink_name>', views.drinks, name="drink_name"), 
+]

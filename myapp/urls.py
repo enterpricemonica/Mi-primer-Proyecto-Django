32c2f8116a298a360973b533560b81fa64ed190d
Monica Santamaria <santamariamonica31@gmail.com>
2026-01-14 01:02:27 -0500

    Creación de direcciones URL y asignación a vistas

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('drinks/<str:drink_name>', views.drinks, name="drink_name"), 
-]
+        path('', views.home, name="home"),
+        path('menu/', views.menu, name="menu"),
+        path('about/', views.about, name="about"),
+        path('book/', views.book, name="book"),
+] 
